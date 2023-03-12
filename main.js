d4fd78cc3cbf8276e724a5839f36781ea8873186
Jacks <codejacks@onmail.com>
2023-03-11 20:18:23 -0600

    First randomized function. Returns 3 images.

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let h1 = document.createElement('h1')
 let hello = document.createTextNode('Hello World')
 h1.appendChild(hello)
 let p = document.createElement('p')
-let s = document.createTextNode('This is where the app will go.')
+let s = document.createTextNode('This is my mythical app.')
 p.appendChild(s)
 app.appendChild(h1)
 app.appendChild(p)
@@ -24,3 +24,32 @@ buttonMaker()
 
 const picArray = []
 const div = document.createElement('div')
+
+//Actual App
+
+const locations = [
+  'img/locations/bioAnomolyPlanets.jpg',
+  'img/locations/fantasyWorld.jpg',
+  'img/locations/giantTornado.jpg',
+  'img/locations/lostStaircase.jpg',
+  'img/locations/meteorStrike.jpg',
+  'img/locations/ruggedPlanet.jpg',
+  'img/locations/spaceExplorer.jpg',
+  'img/locations/spaceMining.jpg',
+  'img/locations/spaceStation.jpg',
+  'img/locations/underwaterCity.jpg'
+]
+const characters = []
+const text = []
+
+const imgRow = document.querySelectorAll('.pics')
+
+
+imgRow.forEach(function(Element) {
+  let x = Math.floor(Math.random() * 10)
+  Element.style.backgroundImage = `url(' ${locations[x]} ')`
+})
+
+// document.querySelector('.pics').style.backgroundImage = "url('" + locations[0] + "')"
+// document.querySelectorAll('.pics')[1].style.backgroundImage = "url('" + locations[7] + "')"
+// document.querySelectorAll('.pics')[2].style.backgroundImage = "url('" + locations[5] + "')"
