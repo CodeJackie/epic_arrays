b1fd57b86568ad5094d26783a827e82eab13afb2
Jacks <codejacks@onmail.com>
2023-03-30 23:23:27 -0500

    CSS issues next

--- main.js
+++ main.js
@@ -86,30 +86,56 @@ matchmaker().then((epicMatch) => {
       const customId = this.getAttribute('data-custom-id')
       console.log(customId)
       
-      let matchFound = false
+      let epicCopy = []
       for (const match of epicMatch) {
-        if (customId === match.id) {
-          epic.push(match)
-          console.log(epic)
-          matchFound = true
-          break
+        if (customId === match.image.id) {
+          epic.push(match) 
+        } else {
+          epicCopy.push(match)
         }
-      }
-      
-      if (!matchFound) {
-        console.log("this didn't work. Something isn't right.")
-      }
+        }
+        if (epic.length === 0) {
+          console.log("This didn't work. Something isn't right.") 
+        } else {
+            if (epic.length > 0) {
+              let x = Math.floor( Math.random() * epic.length )
+              //take the random narrative and map the data to the appropriate html elements
+              const nar = document.getElementById('narrative')
+              const h2 = document.createElement('h2')
+              const div = document.createElement('div')
+              const p = document.createElement('p')
+              const flex = document.createElement('div')//.classList.add('content')
+              const flexy1 = document.createElement('div')
+              const flexy2 = document.createElement('div')//.classList.add('textContent')
+              const title = document.createTextNode(epic[x].title)
+              const content = document.createTextNode(epic[x].content)
+              const date = document.createTextNode(epic[x].date)
+              const img = epic[x].image.imgUrl //returns [object]
+              flexy1.style.backgroundImage = `url('${img}')`
+              flex.appendChild(flexy1)
+              h2.appendChild(title)
+              flexy2.appendChild(h2)
+              p.appendChild(date)
+              flexy2.appendChild(p)
+              div.appendChild(content)
+              flexy2.appendChild(div)
+              flex.appendChild(flexy2)
+              nar.appendChild(flex)
+              flex.classList.add('content')
+              flexy2.classList.add('textContent')
+              nar.style.display = 'block'
+              
+              
+              epicCopy = []     
+          }
+        }
+      })
     })
   })
 
-})
 
 
 
- // const nar = document.getElementById('narrative')
-    // const title = document.createElement('h2')
-    // const content = document.createElement('div')
-    // const xbox = document.createElement('p')
 //On Click, select one of the matching stories to the image id
 //On click, match the ids in StoryArray to the Data Attribute of the selected story
 //On Click, set data attribute as variable. EpicArray.forEach(item() => { if(data.val === EpicArray.id) { epic.push(item)}})
