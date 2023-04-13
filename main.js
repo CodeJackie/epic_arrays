332874e40fb88427e05e43478758159e96bee001
Code Jacks <codejacks@onmail.com>
2023-04-12 22:17:37 -0500

    New layout, added footer, light css work

--- main.js
+++ main.js
@@ -9,27 +9,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-const app = document.getElementById('app')
-let h1 = document.createElement('h1')
-let hello = document.createTextNode('Hello World')
-h1.appendChild(hello)
-let p = document.createElement('p')
-let s = document.createTextNode('Select an image to learn its tale.')
-p.appendChild(s)
-app.appendChild(h1)
-app.appendChild(p)
-
-function buttonMaker() {
-  const button = document.createElement('button')
-  const text = document.createTextNode('I serve no purpose')
-  button.setAttribute('name', 'push me')
-  button.appendChild(text)
-  app.appendChild(button)
-
-}
-
-buttonMaker()
-
 const picArray = []
 const div = document.createElement('div')
 
